b36ce31f58d02df74baf9e53ecee0c5011d0e846
Aris Setiawan <arissetia.m@gmail.com>
2017-09-05 14:22:22 +0700

    neat: save metabox

--- commerce.php
+++ commerce.php
@@ -23,11 +23,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 class CommerceButton {
 
 	/**
-	 * Disini untuk meletakan variabel konstan.
+	 * Disini untuk menambahkkan list tombol.
 	 *
-	 * @var $list_commerce array Definisikan tombol apa saja yang ingin dibuat.
+	 * @var array $list_commerce.
 	 */
 	protected $list_commerce = [ 'bukalapak', 'tokopedia' ];
+
+	/**
+	 * Prefix.
+	 *
+	 * @var string $prefix.
+	 */
+	protected $prefix = 'cmbtn_';
 
 	/**
 	 * Construct saat pemanggilan class
@@ -37,6 +44,7 @@ class CommerceButton {
 		// Jika ada action/tindakan itu hanya untuk didalam admin, maka kita perlu melakukan hal ini, agar efisiensi memory usage tidak banyak.
 		if ( is_admin() ) {
 			add_action( 'add_meta_boxes', array( $this, 'register_metabox_button' ) ); // Ini adalah cara untuk memanggil didalam class.
+			add_action( 'save_post', array( $this, 'save_commerce_button' ) ); // aksi untuk menyimpan post.
 		}
 	}
 
@@ -63,23 +71,55 @@ class CommerceButton {
 	 * @param object $post get data from $post edit.
 	 */
 	public function cb_commerce_button( $post ) {
-		$prefix = 'cmbtn_'; // Gunakan prefix agar terhindar dari custom meta yang memiliki nama yang sama (menghindari bentrok dengan plugin yang memiliki nama field yang sama).
-		$values = get_post_custom( $post->id ); // Ambil custom meta dengan ini.
 
+		$values = get_post_custom( $post->id ); // Ambil custom meta dengan ini.
 		// We'll use this nonce field later on when saving. coba cari tahu apa itu wp_nonce.
-		wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' );
+		wp_nonce_field( $this->prefix . 'my_nonce', $this->prefix . 'metabox_nonce' );
 
 		foreach ( $this->list_commerce as $commerce ) :
-			$meta_name = $prefix . $commerce;
-			$val = isset( $values[ $meta_name ] ) ? esc_attr( $values[ $meta_name ] ) : ''; // Inline if, dan pengecekan apakah field meta kosong atau tidak!
+			$meta_name = $this->prefix . $commerce;
+			$val = isset( $values[ $meta_name ][0] ) ? esc_attr( $values[ $meta_name ][0] ) : ''; // Inline if, dan pengecekan apakah field meta kosong atau tidak!
 			?>
 			<p>
 				<label for="<?php echo esc_attr( $meta_name ); ?>">alamat produk di <?php echo esc_html( $commerce ); ?> </label><br>
-				<input type="text" name="<?php echo esc_attr( $meta_name ); ?>" id="<?php echo esc_attr( $meta_name ); ?>">
+				<input type="text" name="<?php echo esc_attr( $meta_name ); ?>" id="<?php echo esc_attr( $meta_name ); ?>" value="<?php echo esc_attr( $val ); ?>" >
 			<p>
 			<?php
 		endforeach;// Gunakan endif, endwhile, endforeach. bila kodingan di dalam dirasa panjang, sehingga akan memudahkan kita untuk melihat isi dari pengulangan ini.
 	}
+
+	/**
+	 * Disini kita akan menyimpan custom metabox.
+	 *
+	 * @param object $post_id disimpan dimana custom meta ini.
+	 */
+	public function save_commerce_button( $post_id ) {
+
+		// Bail if we're doing an auto save.
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+
+		// If our nonce isn't there, or we can't verify it, bail.
+		if ( ! isset( $_POST[ $this->prefix . 'metabox_nonce' ] ) || ! wp_verify_nonce( $_POST[ $this->prefix . 'metabox_nonce' ], $this->prefix . 'my_nonce' ) ) {
+			return;
+		}
+
+		// Disini kita akan melakukan save meta.
+		foreach ( $this->list_commerce as $commerce ) {
+			$meta_name = $this->prefix . $commerce;
+			$value = $_POST[ $meta_name ];
+			if ( isset( $value ) ) {
+				update_post_meta(
+					$post_id, // post_id
+					$meta_name, // meta_key ini sama seperti nama field pada table
+					sanitize_text_field( $value ), // meta_value isi dari tiap field, coba pelajari sanitize
+					''  // prev_value kita cukup mengkosongkan previous value, karena kita tidak akan menggunakan ini.
+				);
+			}
+		}
+	}
+
 
 }
 
